1bf0322507d6170117d66574e0c89ce0f90010dd
mackfinkel <mackfinkel@gmail.com>
2018-03-07 15:37:09 -0500

    numbers are doubles, text is strings

--- education/downloadTabn104.20.R
+++ education/downloadTabn104.20.R
@@ -14,22 +14,30 @@ setwd(newDir)
 # download and load file
 url <- "https://nces.ed.gov/programs/digest/d17/tables/xls/tabn104.20.xls"
 download.file(url, 'tabn104.20.xls', mode="wb")
-tabn104.20 <- read.xls('tabn104.20.xls')
+tabn104.20 <- data.frame(read.xls('tabn104.20.xls'))
 
 # convert to characters
-tabn104.20 <-  sapply(tabn104.20, function(x) as.character(x))
+tabn104.20[] <- lapply(tabn104.20, as.character)
+#tabn104.20 <-  sapply(tabn104.20, function(x) as.character(x))
 
 # remove NA columns, empty columns
-tabn104.20 <- gsub("!|†|---|‡|[(]|[)]", "", tabn104.20)
+tabn104.20 <- data.frame(lapply(tabn104.20, function(x) {gsub("!|†|---|‡|[(]|[)]", "", x)}))
+
+# tabn104.20 <- gsub("!|†|---|‡|[(]|[)]", "", tabn104.20)
 tabn104.20[tabn104.20 == ""] <- NA
 tabn104.20 <-  tabn104.20[,colSums(is.na(tabn104.20))<nrow(tabn104.20)]
 
 # remove footnotes
-tabn104.20 <- gsub("\\\\", "_", tabn104.20)
-tabn104.20 <- gsub("_[[:digit:]]_", "", tabn104.20)
+tabn104.20 <- data.frame(lapply(tabn104.20, function(x) {gsub("\\\\", "_", x)}))
+tabn104.20 <- data.frame(lapply(tabn104.20, function(x) {gsub("_[[:digit:]]_", "", x)}))
+#tabn104.20 <- gsub("\\\\", "_", tabn104.20)
+#tabn104.20 <- gsub("_[[:digit:]]_", "", tabn104.20)
+
 
 # get years isolated in column 1
+tabn104.20[,1] <- as.character(tabn104.20[,1])
 tabn104.20[,1][grepl("[[:digit:]]{4}", tabn104.20[,1])] <- gsub("[.]|[[:space:]]", "", tabn104.20[,1][grepl("[[:digit:]]{4}", tabn104.20[,1])])
+
 
 # remove first row containing parenthetical note
 tabn104.20 <- tabn104.20[-1,]
@@ -38,6 +46,7 @@ tabn104.20 <- tabn104.20[-1,]
 tabn104.20[1,1] <- "Year"
 
 # combine labeling rows
+tabn104.20[] <- lapply(tabn104.20, as.character)
 tabn104.20[1:2,][is.na(tabn104.20[1:2,])] <- ""
 tabn104.20[1,] <- paste0(tabn104.20[1,], tabn104.20[2,])
 tabn104.20 <- tabn104.20[-2,]
@@ -51,7 +60,9 @@ tabn104.20[1,][tabn104.20[1,] == ""] <- "SE"
 # make row 1 column names, convert to data frame
 colnames(tabn104.20) <- tabn104.20[1,]
 tabn104.20 <- tabn104.20[-1,]
-tabn104.20 <- data.frame(tabn104.20)
+
+# regulate factors to numbers
+tabn104.20[,-1] <- lapply(tabn104.20[,-1], as.double)
 
 # set "group" and "education" to convert column 1 categories to their own columns
 tabn104.20$Group <- NA
@@ -67,7 +78,7 @@ for (i in nonYearsIdx[3:length(nonYearsIdx)]) {
   if (nonYears[i + 1] & !nonYears[i+2]) {
     tabn104.20$Group[groupIdx:i-1] <- group
     group <- tabn104.20[i,1]
-    groupIdx <- i+5
+    groupIdx <- i+3
   } else if (!nonYears[i + 1] & !nonYears[i+2]){
     tabn104.20$Education[educationIdx:i-1] <- education
     education <- tabn104.20[i,1]
